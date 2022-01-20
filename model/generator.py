e7f89d7e0b3d1f1277053e6c66300fb4d7b557e3
kareem <kareem.janou@gmail.com>
2022-01-20 12:28:08 +0100

    generator

--- model/generator.py
+++ model/generator.py
@@ -1,8 +1,9 @@
-import torch.nn as nn
-from pointmixer import PointMixer
-from featureinteraction import FeatureInteraction
-from decoder import Decoder
-from pdsac import PDSAC
+from torch import nn
+import torch
+from model.pointmixer import PointMixer
+from model.featureinteraction import FeatureInteraction
+from model.decoder import Decoder
+from model.pdsac import PDSAC
 
 
 class Generator(nn.Module):
@@ -17,6 +18,12 @@ class Generator(nn.Module):
         self.decoder = Decoder()
         self.pdsac = PDSAC()
 
-    def forward(self, x):
-        # TODO
-        return x
+    def forward(self, cloud_a, cloud_b):
+
+        fusion1, fusion2 = self.encoder(cloud_a, cloud_b)
+        Ag = self.decoder(fusion1)
+        Bg = self.decoder(fusion2)
+        t_a = self.pdsac(cloud_a, Ag)
+        t_b = self.pdsac(cloud_b, Bg)
+        t_e = torch.inverse(t_a) @ t_b
+        return Ag, Bg, t_e
