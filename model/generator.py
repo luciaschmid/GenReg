7315aea72128edd53ecee981eddd1cc9b287ecb4
kareem <kareem.janou@gmail.com>
2022-01-20 15:50:41 +0100

    inverse matrix and generator

--- model/generator.py
+++ model/generator.py
@@ -4,7 +4,7 @@ from model.pointmixer import PointMixer
 from model.featureinteraction import FeatureInteraction
 from model.decoder import Decoder
 from model.pdsac import PDSAC
-
+from utils.invmat import InvMatrix
 
 class Generator(nn.Module):
     def __init__(self):
@@ -17,6 +17,7 @@ class Generator(nn.Module):
         )
         self.decoder = Decoder()
         self.pdsac = PDSAC()
+        self.inverse = InvMatrix()
 
     def forward(self, cloud_a, cloud_b):
 
@@ -25,5 +26,5 @@ class Generator(nn.Module):
         Bg = self.decoder(fusion2)
         t_a = self.pdsac(cloud_a, Ag)
         t_b = self.pdsac(cloud_b, Bg)
-        t_e = torch.inverse(t_a) @ t_b
+        t_e = self.inverse(t_a).bmm(t_b)
         return Ag, Bg, t_e
